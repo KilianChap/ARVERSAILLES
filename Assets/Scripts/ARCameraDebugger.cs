7deff66d1ecd371303607f7f9f4f10ea323b3168
KilianChap <kilian.chapelle@ecole-hexagone.com>
2025-06-16 23:58:20 +0200

    tentative de fix de la detection de l'image

---
+++ Assets/Scripts/ARCameraDebugger.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+using UnityEngine.XR.ARSubsystems;
+
+[RequireComponent(typeof(ARCameraManager))]
+public class ARCameraDebugger : MonoBehaviour
+{
+    private ARCameraManager cameraManager;
+
+    void Awake()
+    {
+        cameraManager = GetComponent<ARCameraManager>();
+    }
+
+    void Start()
+    {
+        Debug.Log("ARCameraDebugger: Test de la caméra AR...");
+
+        if (cameraManager == null)
+        {
+            Debug.LogError("ARCameraManager non trouvé !");
+            return;
+        }
+
+        cameraManager.frameReceived += OnCameraFrameReceived;
+    }
+
+    private void OnCameraFrameReceived(ARCameraFrameEventArgs args)
+    {
+        if (cameraManager.TryAcquireLatestCpuImage(out XRCpuImage image))
+        {
+            Debug.Log($"Frame reçue : {image.width} x {image.height}");
+            image.Dispose();
+        }
+        else
+        {
+            Debug.LogWarning("Aucun accès à l'image CPU !");
+        }
+    }
+}
