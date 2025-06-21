b0c76a33088e257d15adf5be33a23c00423b1c82
KilianChap <kilian.chapelle@ecole-hexagone.com>
2025-06-21 10:47:41 +0200

    modification du code pour que les deux modeles 3d de la marie apparaissent avec leurs image de trigger respective

--- Assets/Scripts/ModelSpawn.cs
+++ Assets/Scripts/ModelSpawn.cs
@@ -7,11 +7,12 @@ public class ModelSpawn : MonoBehaviour
 {
     public List<GameObject> modelList = new List<GameObject>();
     public GameObject GareSpawn;
-    public GameObject MairieSpawn;
+    public GameObject MairieSpawn1897;
+    public GameObject MairieSpawn1907;
 
     private Dictionary<string, GameObject> spawnedModels = new Dictionary<string, GameObject>();
 
-    // Appelé automatiquement par Unity depuis l'inspecteur
+    // Appelle automatiquement par Unity depuis l'inspecteur
     public void OnTrackablesChanged(ARTrackablesChangedEventArgs<ARTrackedImage> eventArgs)
     {
         foreach (var trackedImage in eventArgs.added)
@@ -22,11 +23,18 @@ public class ModelSpawn : MonoBehaviour
             {
                 foreach (var model in modelList)
                 {
-                    if (model.name == "mairie1907" && imageName == "A")
+                    if (model.name == "mairie1897" && imageName == "A")
                     {
-                        GameObject instance = Instantiate(model, MairieSpawn.transform.position, MairieSpawn.transform.rotation);
+                        GameObject instance = Instantiate(model, MairieSpawn1897.transform.position, MairieSpawn1897.transform.rotation);
                         spawnedModels.Add(imageName, instance);
-                        Debug.Log("mairie1907 instanciée via l'image A");
+                        Debug.Log("mairie1897 instanciee via l'image A");
+                        break;
+                    }
+                    if (model.name == "mairie1907" && imageName == "B")
+                    {
+                        GameObject instance = Instantiate(model, MairieSpawn1907.transform.position, MairieSpawn1907.transform.rotation);
+                        spawnedModels.Add(imageName, instance);
+                        Debug.Log("mairie1907 instanciee via l'image B");
                         break;
                     }
                 }
